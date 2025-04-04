c1661038a57b93b4c202e9bcc740e3a5739bee94
PreuLittleSiberian <preulittlesiberian@gmail.com>
2025-04-04 18:49:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 function toggleMenu() {
-    document.querySelector("nav ul").classList.toggle("show");
+    let menu = document.getElementById("menu");
+    menu.classList.toggle("visible");
+    menu.classList.toggle("hidden");
 }
-
-// Para móviles: abrir y cerrar submenús con clic
-document.querySelectorAll(".dropdown > a").forEach(link => {
-    link.addEventListener("click", function(event) {
-        event.preventDefault(); // Evita que el enlace se active
-        let parent = this.parentElement;
-        parent.classList.toggle("active");
-    });
-});
 
 function verMas() {
     alert("¡Más información próximamente!");
